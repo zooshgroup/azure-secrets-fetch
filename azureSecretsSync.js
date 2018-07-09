0951169782b0df5498c6fc96c7325dcc3bd6b2c6
Balázs Czap <balazsczap@zooshgroup.com>
2018-07-09 15:16:33 +0200

    Fix path resolve process

--- azureSecretsSync.js
+++ azureSecretsSync.js
@@ -1,16 +1,19 @@
 const getParametersFromVault = require('./azureSecrets.js');
 const execFileSync = require('exec-file-sync');
+const path = require('path');
 
 if (process.argv[2]) {
   (async () => {
-    const path = process.argv[2];
-    const params = await getParametersFromVault(path);
+    const vaultUri = process.argv[2];
+    const params = await getParametersFromVault(vaultUri);
     console.log(JSON.stringify(params));
   })();
 }
 
-const getParametersFromVaultSync = (path) => {
-  const execResult = execFileSync(process.argv0, ['./azureSecretsSync.js', path]).toString();
+
+const getParametersFromVaultSync = (vaultUri) => {
+  const runTarget = path.resolve(__dirname, 'azureSecretsSync.js');
+  const execResult = execFileSync(process.argv0, [runTarget, vaultUri]).toString();
   return JSON.parse(execResult);
 };
 
